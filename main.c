08f870123590d01fbde83206d05e8eb00db966d2
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-07-04 09:51:06 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <xc.h>
+
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (Power-up Timer is disabled)
+#pragma config CP = OFF         // Code Protection bit (Code protection disabled)
+
+#define _XTAL_FREQ 4000000
+
+
+void main(void) {
+    TRISA = 0x00;
+    PORTA = 0x00;
+    TRISB = 0x01;
+    PORTB = 0x00;
+    unsigned char sample = 0;
+    while(1){
+        RA0 = 0;
+        int i;
+        for (i = 0; i < 9; i++ ){
+            RA1 = 0;
+            __delay_us(50);
+            sample <<= 1;
+            sample += RB0;
+            RA1 = 1;
+            __delay_us(50); // total delay 100 us 10kHz clk frequency 
+        }
+        RA0 = 1;
+        RA1 = 0;
+        __delay_us(1);
+    }
+    return;
+}
